cd9e22f96dc9f3809a2f5792be46827b6e595183
Hong-Liang <57203229+Hong-Liang@users.noreply.github.com>
2020-08-20 12:49:24 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p13_demo_tablelayout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.myapplicationdev.android.p13_demo_tablelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnOnClick(view : View) {
+        var msg = ""
+        val btnS = view as Button
+
+        when (btnS.id) {
+            button1.id -> msg = "Button 1 is clicked"
+            button2.id -> msg = "Button 2 is clicked"
+            button3.id -> msg = "Button 3 is clicked"
+            button4.id -> msg = "Button 4 is clicked"
+            button5.id -> msg = "Button 5 is clicked"
+        }
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+    }
+}
